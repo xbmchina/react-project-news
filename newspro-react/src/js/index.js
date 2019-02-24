e5133123a5e6c48c6a198e1fdcab67a83f2896b9
xbmchina <1131663064@163.com>
2019-02-24 15:01:06 +0800

    first commit

---
+++ newspro-react/src/js/index.js
@@ -0,0 +1,18 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+class Index extends React.Component {
+
+  render() {
+    return (
+      <div>
+        1234
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(
+  <Index />, document.getElementById('app'));
